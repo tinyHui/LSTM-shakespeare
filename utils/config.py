e429988ddcb7c79b32294501635adbd8b084d072
Jason Chen <jason_chenjh@hotmail.com>
2017-07-22 16:06:54 +0100

    move constant config to a seperate file

---
+++ utils/config.py
@@ -0,0 +1,14 @@
+COMEDY_FULL_TEXT = "./data/full-text.txt"
+TOKEN_INDEX_MAP = "./bin/token_index.map"
+INDEX_TOKEN_MAP = "./bin/index_token.map"
+CHECKOUT_FOLDER = "./bin/checkpoint"
+
+TOKEN_NUMBER = 38778
+SENTENCES = 125986
+MAX_LENGTH = 27
+SKIP_STEP = 3
+
+BATCH_SIZE = 100
+EMBEDDING_SIZE = 100
+SKIP_WINDOW = 3
+OUTPUT_KEEP_PROB = 0.5
