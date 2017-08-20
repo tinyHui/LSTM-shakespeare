a58305ed81a6999da43b8b8c5142d2962b0f6fb2
Jason Chen <jason_chenjh@hotmail.com>
2017-08-20 15:09:34 +0100

    add session store function for backup interrupted training

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 from model import Generator, build_network
 import tensorflow as tf
 import logging
+import os
 
 from utils.config import COMEDY_FULL_TEXT, BATCH_SIZE, SENTENCE_LENGTH
+
+tf.app.flags.DEFINE_string('checkpoints_dir', os.path.abspath('./checkpoints/'), 'checkpoints save path.')
+tf.app.flags.DEFINE_string('model_prefix', 'shakespeare', 'model save prefix.')
+FLAGS = tf.app.flags.FLAGS
 
 logging.basicConfig(format="%(levelname)s | %(asctime)s - %(message)s",
                     level=logging.DEBUG)
@@ -17,25 +22,45 @@ def train():
 
     generator = Generator(COMEDY_FULL_TEXT, BATCH_SIZE, SENTENCE_LENGTH)
 
-    for epoch in range(EPOCH):
-        while generator.have_next():
-            sentences = generator.next_batch()
-            following_tokens = generator.following_tokens()
+    saver = tf.train.Saver(tf.global_variables())
 
-            loss_value, _, _ = sess.run([loss, last_state, train_op], feed_dict={
-                inputs: sentences,
-                expect_tokens: following_tokens
-            })
+    start_epoch = 0
+    epoch = 0
 
-            logging.info(f"Processed epoch: {epoch + 1}, batch: {generator.get_iter_count()} | loss: {loss_value}")
+    checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoints_dir)
+    if checkpoint:
+        logging.info("Found checkoutpoint, trying to restore from it")
+        saver.restore(sess, checkpoint)
+        logging.info("Restored successfully")
+        start_epoch += int(checkpoint.split('-')[-1])
 
-        generator.reset_iterator()
+    logging.info('start training')
+    try:
+        for epoch in range(start_epoch, EPOCH):
+            while generator.have_next():
+                sentences = generator.next_batch()
+                following_tokens = generator.following_tokens()
+
+                loss_value, _, _ = sess.run([loss, last_state, train_op], feed_dict={
+                    inputs: sentences,
+                    expect_tokens: following_tokens
+                })
+
+                logging.info(f"Processed epoch: {epoch + 1}, batch: {generator.get_iter_count()} | loss: {loss_value}")
+
+            generator.reset_iterator()
+
+            if epoch % 5 == 0:
+                logging.info(f"Model saved in epoch {epoch}")
+                saver.save(sess, os.path.join(FLAGS.checkpoints_dir, FLAGS.model_prefix), global_step=epoch)
+    except KeyboardInterrupt:
+        logging.info("Interrupted, try saving current training as a checkpoint")
+        saver.save(sess, os.path.join(FLAGS.checkpoints_dir, FLAGS.model_prefix), global_step=epoch)
+        logging.info(f"Last epoch were saved, next time will start from epoch {epoch}")
 
 
 if __name__ == '__main__':
     sess = tf.InteractiveSession()
-
     train()
-
     sess.close()
 
--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,10 @@
-COMEDY_FULL_TEXT = "./data/partial-text.txt"
-# COMEDY_FULL_TEXT = "./data/full-text.txt"
+# COMEDY_FULL_TEXT = "./data/partial-text.txt"
+COMEDY_FULL_TEXT = "./data/full-text.txt"
 TOKEN_INDEX_MAP = "./bin/token_index.map"
 INDEX_TOKEN_MAP = "./bin/index_token.map"
 CHECKOUT_FOLDER = "./bin/checkpoint"
 
-TOKEN_NUMBER = 5679
+TOKEN_NUMBER = 19416
 BATCH_SIZE = 64
 EMBEDDING_SIZE = 128
 SENTENCE_LENGTH = 30
